f9184afe1d9be223ec62eb22bbd05699f4100a11
surface <1074133056@qq.com>
2022-05-25 09:12:14 +0800

    现将头部text view填充完成，将存储位置的数据已读取。 下一步： 将数据写入list view当中

--- app/src/main/java/com/example/booker/MainActivity.java
+++ app/src/main/java/com/example/booker/MainActivity.java
@@ -1,17 +1,10 @@
-package com.example.booker;                         //架构重盖：
+package com.example.booker;
 /*在程序运行开始前读取一次磁盘数据，加载至映射数组中，（前后仅两次操作文件）
-        ！！！在程序内部运行时，都是对映射的数组单元做操作，最后一次性写入！！！！！
-
         当使用操作界面进行时，不能存储之后日期的数据，如要存则提醒不能，如此带来的好处是：可先进行一次判断是否为当前日期的存储，若是，可直接在映射数组最后添加，不是，则需要进行判断，总之在映射数组中始终是以时间为标准的一个筛选结构
-
-        程序彻底关闭后在Ondestory 里将映射数组中的数据再次写入文件
 
         结论：
         问题部分：
-        1.那么第一个问题就是如何在Activity之间传递当前的映射数组
-        2.如何使用OnDestory回调函数
-        论述部分：
-        数组，与链表的优劣正式有了一个概念*/
+        1.那么第一个问题就是如何在Activity之间传递当前的映射数组*/
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,6 +15,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -32,6 +26,10 @@ import java.time.LocalDate;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    //////////////////////////当前周or上周的总体数据,但不确定是否为6各一组？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？
+    private final int WIDTH_OF_DATA_SET=6;
+    private String[][] node=new String[100][WIDTH_OF_DATA_SET];
+    private int count=0;
 
     private ExpandableListView listView;
 
@@ -42,8 +40,12 @@ public class MainActivity extends AppCompatActivity {
     /*文件类用于读取分布类型数据*/
     private File file;
     /*该字符串组即是用来指向文件地址的*///???????????????????????????????????????????????????
-    private String[] paths={"time\\month\\first_week.txt","category\\content\\"};
-    private String path_count="count.txt";//具体的顺序为，总支出，总收入，总差额
+    private String paths="time\\month\\first_week.txt";
+    private String month_in;
+    private String week_of_month;
+    private final String path_count="count.txt";//具体的顺序为，总支出，总收入，总差额
+    /*当前日期*/
+    private Calendar date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,10 +53,14 @@ public class MainActivity extends AppCompatActivity {
 
         this.Init_Core();
 
+        date=Calendar.getInstance();
+        month_in=String.valueOf(date.get(Calendar.MONTH));//直接获取当前日期月份的值？？
+        week_of_month=String.valueOf(date.get(Calendar.WEEK_OF_MONTH));//先假定返回一个阿拉伯数字！！！！！！！！！！！！！！！！！！！！！
+
         this.money_text();
 
-        LocalDate date=LocalDate.now();
-        file=new File(getFilesDir(),paths[0]);
+        this.read_by();
+        //下一步就该将node里的数据写入list view中了。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。
 
 
 
@@ -72,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    /*初始化控件*/
     private void Init_Core(){
         listView=findViewById(R.id.My_list);
         x=(Button) findViewById(R.id.record_B);
@@ -80,17 +87,76 @@ public class MainActivity extends AppCompatActivity {
         come=findViewById(R.id.receive);
         to=findViewById(R.id.count);
     }
-    private void money_text(){
-        file=new File(getFilesDir(),path_count);
+    /*头部三个文本框通过外界文件写入*/
+    private void money_text() {
+        file = new File(getFilesDir(), "time\\" + month_in + "月\\" + path_count);
+        if (file.exists()) {
+        } else {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                Log.i("create", "NO");
+            }
+        }
         try {
-            BufferedReader Br=new BufferedReader(new FileReader(file));
+            BufferedReader Br = new BufferedReader(new FileReader(file));
             go.setText(Br.readLine());      //鲁棒性很差？？？？？？？？？？？？？，具体来说并没有一种安全机制使得一定读到所需内容？？？？？？？？？？？？？？？？？？
             come.setText(Br.readLine());
             to.setText(Br.readLine());
+            Br.close();
         } catch (FileNotFoundException e) {
-            Log.i("Br","can't read");
+            Log.i("Br", "can't read");
         } catch (IOException e) {
-            Log.i("readline","error");
+            Log.i("readline", "error");
+        }
+    }
+    /*扩充存储单元*/
+    private String[][] expand(int length){//////////////起名问题？？？？？？？？？？？？？？？？？？？？？？？？？？？？
+        String[][] re=new String[length+100][WIDTH_OF_DATA_SET];
+        for(int i=0;i<length;i++){
+            re[i]=node[i];
+        }
+        return re;
+    }
+    /*详细信息读取*/
+    private void read_by(){
+
+        file=new File(getFilesDir(),"time\\"+month_in+"月\\"+week_of_month+"_weeksequence.txt");
+        if(file.exists()){
+        }else{
+            file=new File(getFilesDir(),"time\\"+month_in+"月\\"+String.valueOf(date.get(Calendar.WEEK_OF_MONTH)-1)+"_weeksequence.txt");
+            if(file.exists()){
+            }else {
+                Toast.makeText(this, "未找到最近的数据", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        /*从文件读取数据*/
+        try {
+            BufferedReader Br=new BufferedReader(new FileReader(file));
+            String t=Br.readLine();
+            while(t!=null){
+                /*读取数据*/
+                try {//try的机制????????????????????????????????????????????????????????????????????????
+                    node[count][0] = t;
+                } catch (IndexOutOfBoundsException e) {
+                    node=this.expand(node.length);
+                    node[count][0] = t;
+                }
+                for (int i = 1; i < WIDTH_OF_DATA_SET; i++) {
+                    node[count][i] = Br.readLine();
+                }
+                count++;
+                /*重复写入以尝试*/
+                t=Br.readLine();
+            }
+            Toast.makeText(this, "complete", Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            Log.i("haha","haha");
+            Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Log.i("hat","not at all");
+            Toast.makeText(this, "fuck", Toast.LENGTH_SHORT).show();
         }
     }
 }
