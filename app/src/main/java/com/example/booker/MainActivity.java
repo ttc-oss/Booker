42187bacf0198666123fde32bee91eadd42f1d36
mine <1074133056@qq.com>
2022-05-26 16:29:57 +0800

    至现在认为能够较好的将数据从内存中读取出来，下一步填充list view

--- app/src/main/java/com/example/booker/MainActivity.java
+++ app/src/main/java/com/example/booker/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.booker;
 /*在程序运行开始前读取一次磁盘数据，加载至映射数组中，（前后仅两次操作文件）
         当使用操作界面进行时，不能存储之后日期的数据，如要存则提醒不能，如此带来的好处是：可先进行一次判断是否为当前日期的存储，若是，可直接在映射数组最后添加，不是，则需要进行判断，总之在映射数组中始终是以时间为标准的一个筛选结构
 
+           比较字符串的哈希值和直接比较字符串？？？？
         结论：
         问题部分：
         1.那么第一个问题就是如何在Activity之间传递当前的映射数组*/
+
+/*默认了数据在文件中时间最近的在开始*/
+/*至现在认为能够较好的将数据从内存中读取出来*/
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,13 +26,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    //////////////////////////当前周or上周的总体数据,但不确定是否为6各一组？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？
-    private final int WIDTH_OF_DATA_SET=6;
-    private String[][] node=new String[100][WIDTH_OF_DATA_SET];
+    //////////////////////////当前周or上周的总体数据,但不确定是否为4各一组？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？
+    private final int WIDTH_OF_DATA_SET=4;
+    /*这里首先默认数据存储时，时间最近的在第一项*/
+    private String[][] node=new String[100][WIDTH_OF_DATA_SET];     //应该可以用内部类包装一下。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。
     private int count=0;
 
     private ExpandableListView listView;
@@ -39,13 +43,14 @@ public class MainActivity extends AppCompatActivity {
 
     /*文件类用于读取分布类型数据*/
     private File file;
-    /*该字符串组即是用来指向文件地址的*///???????????????????????????????????????????????????
+
     private String paths="time\\month\\first_week.txt";
     private String month_in;
     private String week_of_month;
     private final String path_count="count.txt";//具体的顺序为，总支出，总收入，总差额
     /*当前日期*/
     private Calendar date;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,12 +60,12 @@ public class MainActivity extends AppCompatActivity {
 
         date=Calendar.getInstance();
         month_in=String.valueOf(date.get(Calendar.MONTH));//直接获取当前日期月份的值？？
-        week_of_month=String.valueOf(date.get(Calendar.WEEK_OF_MONTH));//先假定返回一个阿拉伯数字！！！！！！！！！！！！！！！！！！！！！
 
-        this.money_text();
+
+
+        this.money_text(this.month_in);
 
         this.read_by();
-        //下一步就该将node里的数据写入list view中了。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。
 
 
 
@@ -88,8 +93,8 @@ public class MainActivity extends AppCompatActivity {
         to=findViewById(R.id.count);
     }
     /*头部三个文本框通过外界文件写入*/
-    private void money_text() {
-        file = new File(getFilesDir(), "time\\" + month_in + "月\\" + path_count);
+    private void money_text(String mon) {
+        file = new File(getFilesDir(), "time\\" + mon + "月\\" + path_count);
         if (file.exists()) {
         } else {
             try {
@@ -118,22 +123,64 @@ public class MainActivity extends AppCompatActivity {
         }
         return re;
     }
-    /*详细信息读取*/
-    private void read_by(){
 
-        file=new File(getFilesDir(),"time\\"+month_in+"月\\"+week_of_month+"_weeksequence.txt");
-        if(file.exists()){
-        }else{
-            file=new File(getFilesDir(),"time\\"+month_in+"月\\"+String.valueOf(date.get(Calendar.WEEK_OF_MONTH)-1)+"_weeksequence.txt");
-            if(file.exists()){
-            }else {
-                Toast.makeText(this, "未找到最近的数据", Toast.LENGTH_SHORT).show();
+
+
+    /*详细信息读取*/
+    private void read_by(){                     //整理解析该函数。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。
+
+        /*这一部分用来判定是否读取文件时，需要切换路径，顺便将”今日“的周数读出*/
+        int compare_week=date.get(Calendar.WEEK_OF_MONTH);
+        week_of_month=String.valueOf(compare_week);//先假定返回一个阿拉伯数字！！！！！！！！！！！！！！！！！！！！！
+        /*布尔式是用来告诉是否文件需要切换*/
+        Boolean file_switch=true;
+        Calendar cdate= (Calendar) date.clone();
+        String item_mon=month_in;
+        for(int j=0;j<3;j++){
+            cdate.add(Calendar.DATE,-j);
+            if(cdate.get(Calendar.WEEK_OF_MONTH)<compare_week){
+                file_switch=false;
+                break;
+            }else if(cdate.get(Calendar.WEEK_OF_MONTH)>compare_week){
+                item_mon=String.valueOf(Integer.parseInt(month_in)-1);
+                file_switch=false;
+                break;
             }
         }
 
-        /*从文件读取数据*/
+
+
+        /*第二部分根据判定开始读取数据*/
+        if(file_switch){
+            file=new File(getFilesDir(),"time\\"+month_in+"月\\"+week_of_month+"_weeksequence.txt");
+            if(file.exists()){
+
+                this.read_core(file);
+
+            }else {
+                Toast.makeText(this, "未找到最近的数据", Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            file=new File(getFilesDir(),"time\\"+month_in+"月\\"+week_of_month+"_weeksequence.txt");
+            if(file.exists()){
+
+                this.read_core(file);
+
+            }else {
+                Toast.makeText(this, "未找到最近的数据", Toast.LENGTH_SHORT).show();
+            }
+            file=new File(getFilesDir(),"time\\"+item_mon+"月\\"+cdate.get(Calendar.WEEK_OF_MONTH)+"_weeksequence.txt");
+        }
+
+    }
+
+
+
+
+
+    private void read_core(File filename){
         try {
-            BufferedReader Br=new BufferedReader(new FileReader(file));
+            BufferedReader Br=new BufferedReader(new FileReader(filename));
             String t=Br.readLine();
             while(t!=null){
                 /*读取数据*/
@@ -149,9 +196,11 @@ public class MainActivity extends AppCompatActivity {
                 count++;
                 /*重复写入以尝试*/
                 t=Br.readLine();
+
+                Br.close();
             }
             Toast.makeText(this, "complete", Toast.LENGTH_SHORT).show();
-        } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {                 //鲁棒性极差。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。
             Log.i("haha","haha");
             Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
@@ -159,4 +208,5 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "fuck", Toast.LENGTH_SHORT).show();
         }
     }
+
 }
